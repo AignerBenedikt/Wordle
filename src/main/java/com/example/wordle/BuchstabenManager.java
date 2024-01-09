57bda35b746fd6a9c0b8e850baee1b41f3b3532e
AignerBenedikt <benedikt.aigner@stud.fh-campuswien.ac.at>
2024-01-09 23:45:13 +0100

    Wordle Beta Angelika, Jochen, Rayen, Fabio und Benedikt. Bei Fragen melden!

---
+++ src/main/java/com/example/wordle/BuchstabenManager.java
@@ -0,0 +1,108 @@
+package com.example.wordle;
+
+import java.util.Arrays;
+
+
+public class BuchstabenManager {
+
+    StringBuilder let =new StringBuilder();
+
+    public int[] comparisonOfLetters (String loesung, String eingabe) {
+
+        let = new StringBuilder();
+        int[] anzeigeArray = new int[loesung.length()];                 // 0 = Zeichen falsch; 1 = Zeichen gelb (vorhanden aber falsche Stelle), 2 = Zeichen richtig grün
+
+
+        if (eingabe.equalsIgnoreCase(loesung)) {
+
+            anzeigeArray = new int[]{2, 2, 2, 2, 2};
+
+
+        } else {
+
+            for (int i = 0; i < loesung.length(); i++) {
+
+                if (eingabe.charAt(i) == loesung.charAt(i)) {// Zeichen ist richtig => anzeigeArray[i] = 2
+
+                    anzeigeArray[i] = 2;
+
+                } else {
+
+                    for (int j = 0; j < loesung.length(); j++) {
+
+                        if (eingabe.charAt(i) == loesung.charAt(j)) {// Zeichen ist vorhanden aber falsche Stelle => anzeigeArray[i] = 1
+
+                            anzeigeArray[i] = 1;
+
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int m = 0; m < loesung.length(); m++) {
+
+            int countletter = countLetter(String.valueOf(eingabe.charAt(m)), loesung);  //Anzahl eines Buchstaben in dem Lösungswort
+
+            if (countletter >= 1) {                                                       //Falls der Buchstabe min 1 Mal vorkommt
+
+                for (int n = 0; n < loesung.length(); n++) {
+
+                    if (n == m) {                                                         //n==m steht für den Buchstabe der abgefragt wird und n stht für alle anderen Buchstaben die im Lösungswort vorkommen wäre die selbe Stelle und das wollen wir ja gar nicht abfragen
+
+                        continue;
+
+                    }
+                    if (eingabe.charAt(m) == eingabe.charAt(n) && anzeigeArray[m] > 0 && anzeigeArray[n] > 0) { // Beide Stellen des Lösungswort müssen gleich sein, beide Stellen dürfen den Wert 0 nicht haben [0 != 0]
+
+                        if (anzeigeArray[m] == 2) {                //Falls das Lösungswort einmal vorkommt oder das
+                            if (countletter == 1){
+                                anzeigeArray[n]=0;
+                            } if (countletter>1){
+                                countletter--;
+                            }
+                        }
+
+                        if (anzeigeArray[m]==1){
+                            if (countletter == 1){
+                                anzeigeArray[n]=0;
+                            }if (countletter>1){
+                                countletter--;
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+
+
+        //return anzeigeFarbausgabeneu(anzeigeArray,eingabe);
+
+
+        return anzeigeArray;
+    }
+
+
+    public int countLetter(String letter, String loesung){
+        int count=0;
+        let = new StringBuilder();
+
+        if (let.toString().contains(letter)){
+            return 0;
+        }
+
+
+        for(int i=0; i< loesung.length();i++){
+            if (String.valueOf(loesung.charAt(i)).equals(letter)) {   // Zeichen ist vorhanden aber falsche Stelle => anzeigeArray[i] = 1
+                count++;
+            }
+        }
+        if (count>0){
+            let.append(letter);
+
+        }
+
+        return count;
+    }
+}
---
+++ src/main/java/com/example/wordle/WordleController.java
@@ -0,0 +1,414 @@
+package com.example.wordle;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.*;
+
+import static javafx.application.Application.launch;
+
+//https://www.youtube.com/watch?v=B4t0Y40U4HI
+
+public class WordleController{
+    WordManager wm = new WordManager();
+    BuchstabenManager bm = new BuchstabenManager();
+    String word = "";
+    List<String> stringList = new ArrayList<>();
+
+    public WordleController() {
+        wm.generateWordList(5);
+        this.word = wm.SolutionWord();
+    }
+
+    public Button Guess1;
+    public Button Guess2;
+    public Button Guess3;
+    public Button Guess4;
+    public Button Guess5;
+    public Button Guess6;
+    public Button Back;
+    public Button Delete;
+    public Button Q;
+    public Button W;
+    public Button E;
+    public Button R;
+    public Button T;
+    public Button Z;
+    public Button U;
+    public Button I;
+    public Button O;
+    public Button P;
+    public Button A;
+    public Button S;
+    public Button D;
+    public Button F;
+    public Button G;
+    public Button H;
+    public Button J;
+    public Button K;
+    public Button L;
+    public Button Y;
+    public Button X;
+    public Button C;
+    public Button V;
+    public Button B;
+    public Button N;
+    public Button M;
+    public TextField guessInput;
+    public Button playAgain;
+    public Button quit;
+    public Label box00 = new Label();
+    public Label box01 = new Label();
+    public Label box02 = new Label();
+    public Label box03 = new Label();
+    public Label box04 = new Label();
+    public Label box10 = new Label();
+    public Label box11 = new Label();
+    public Label box12 = new Label();
+    public Label box13 = new Label();
+    public Label box14 = new Label();
+    public Label box20 = new Label();
+    public Label box21 = new Label();
+    public Label box22 = new Label();
+    public Label box23 = new Label();
+    public Label box24 = new Label();
+    public Label box30 = new Label();
+    public Label box31 = new Label();
+    public Label box32 = new Label();
+    public Label box33 = new Label();
+    public Label box34 = new Label();
+    public Label box40 = new Label();
+    public Label box41 = new Label();
+    public Label box42 = new Label();
+    public Label box43 = new Label();
+    public Label box44 = new Label();
+    public Label box50 = new Label();
+    public Label box51 = new Label();
+    public Label box52 = new Label();
+    public Label box53 = new Label();
+    public Label box54 = new Label();
+
+    @FXML
+    protected void setDelete() {
+        guessInput.clear();
+    }
+    @FXML
+    protected void playAgain() throws IOException {
+        Stage stage = new Stage();
+        FXMLLoader fxmlLoader = new FXMLLoader(WordleApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Wordle");
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void allguessButtonDisabled() {
+        Guess1.setDisable(true);
+        Guess2.setDisable(true);
+        Guess3.setDisable(true);
+        Guess4.setDisable(true);
+        Guess5.setDisable(true);
+        Guess6.setDisable(true);
+    }
+    @FXML
+    protected void quit() {
+        System.exit(0);
+    }
+    @FXML
+    protected void setBack() {
+        try {
+            guessInput.setText(guessInput.getText().substring(0, guessInput.getLength() - 1));
+        } catch (StringIndexOutOfBoundsException e) {
+
+        }
+    }
+
+    @FXML
+    protected void Letter(ActionEvent actionEvent) {
+
+        if (guessInput.getText().length()==5){
+            return;
+        }
+        Character text = actionEvent.getTarget().toString().charAt(10);
+        guessInput.appendText(String.valueOf(text));
+    }
+    @FXML
+    protected void checkGuess1() {
+        Guess1.setDisable(true);
+        String guess = guessInput.getText().toUpperCase();
+        Label[] row0 = {box00, box01, box02, box03, box04};
+        System.out.println(word);
+
+
+        if (!wm.wordExist(guess)){
+            guessInput.clear();
+            Guess1.setDisable(false);
+            System.out.println("FALSCH");
+            return;
+        }
+
+        stringList.add(guess);
+
+        int [] array=bm.comparisonOfLetters(word,guess);
+        colorBoxes(array,guess,row0);
+        Guess2.setDisable(false);
+
+        if (word.equals(guess)){
+            allguessButtonDisabled();
+            playAgain.setVisible(true);
+            quit.setVisible(true);
+            return;
+        }
+
+        guessInput.clear();
+    }
+    @FXML
+    protected void checkGuess2() {
+        Guess2.setDisable(true);
+        String guess = guessInput.getText().toUpperCase();
+        System.out.println(word);
+
+        if (!wm.wordExist(guess)){
+            guessInput.clear();
+            Guess2.setDisable(false);
+            System.out.println("FALSCH");
+            return;
+        }
+
+        if (stringList.contains(guess)){
+            guessInput.clear();
+            Guess2.setDisable(false);
+            return;
+        }else {
+            stringList.add(guess);
+        }
+
+        Label[] row1 = {box10, box11, box12, box13, box14};
+        int [] array=bm.comparisonOfLetters(word,guess);
+        colorBoxes(array,guess,row1);
+        Guess3.setDisable(false);
+
+        if (word.equals(guess)){
+            allguessButtonDisabled();
+            playAgain.setVisible(true);
+            quit.setVisible(true);
+            return;
+        }
+
+        guessInput.clear();
+    }
+    @FXML
+    protected void checkGuess3() {
+        Guess3.setDisable(true);
+        String guess = guessInput.getText().toUpperCase();
+        if (!wm.wordExist(guess)){
+            guessInput.clear();
+            Guess3.setDisable(false);
+            System.out.println("FALSCH");
+            return;
+        }
+
+        if (stringList.contains(guess)){
+            guessInput.clear();
+            Guess3.setDisable(false);
+            return;
+        }else {
+            stringList.add(guess);
+        }
+
+
+
+        Label[] row2 = {box20, box21, box22, box23, box24};
+        int [] array=bm.comparisonOfLetters(word,guess);
+        colorBoxes(array,guess,row2);
+        Guess4.setDisable(false);
+
+        if (word.equals(guess)){
+            allguessButtonDisabled();
+            playAgain.setVisible(true);
+            quit.setVisible(true);
+            return;
+        }
+
+        guessInput.clear();
+    }
+    @FXML
+    protected void checkGuess4() {
+        Guess4.setDisable(true);
+        String guess = guessInput.getText().toUpperCase();
+
+        if (!wm.wordExist(guess)){
+            guessInput.clear();
+            Guess4.setDisable(false);
+            System.out.println("FALSCH");
+            return;
+        }
+
+        if (stringList.contains(guess)){
+            guessInput.clear();
+            Guess4.setDisable(false);
+            return;
+        }else {
+            stringList.add(guess);
+        }
+
+
+
+        Label[] row3 = {box30, box31, box32, box33, box34};
+        int [] array=bm.comparisonOfLetters(word,guess);
+        colorBoxes(array,guess,row3);
+        Guess5.setDisable(false);
+
+        if (word.equals(guess)){
+            allguessButtonDisabled();
+            playAgain.setVisible(true);
+            quit.setVisible(true);
+            return;
+        }
+
+        guessInput.clear();
+    }
+    @FXML
+    protected void checkGuess5() {
+        Guess5.setDisable(true);
+        String guess = guessInput.getText().toUpperCase();
+
+        if (!wm.wordExist(guess)){
+            guessInput.clear();
+            Guess5.setDisable(false);
+            System.out.println("FALSCH");
+            return;
+        }
+
+        if (stringList.contains(guess)){
+            guessInput.clear();
+            Guess5.setDisable(false);
+            return;
+
+        }else {
+            stringList.add(guess);
+        }
+
+
+        Label[] row4 = {box40, box41, box42, box43, box44};
+        int [] array=bm.comparisonOfLetters(word,guess);
+        colorBoxes(array,guess,row4);
+        Guess6.setDisable(false);
+
+        if (word.equals(guess)){
+            allguessButtonDisabled();
+            playAgain.setVisible(true);
+            quit.setVisible(true);
+            return;
+        }
+
+        guessInput.clear();
+    }
+    @FXML
+    protected void checkGuess6() {
+
+        Guess6.setDisable(true);
+        String guess = guessInput.getText().toUpperCase();
+
+        if (!wm.wordExist(guess)){
+            guessInput.clear();
+            Guess6.setDisable(false);
+            System.out.println("FALSCH");
+            return;
+        }
+
+        if (stringList.contains(guess)){
+            guessInput.clear();
+            Guess6.setDisable(false);
+            return;
+        }else {
+            stringList.add(guess);
+        }
+
+        Label[] row5 = {box50, box51, box52, box53, box54};
+        int [] array=bm.comparisonOfLetters(word,guess);
+        colorBoxes(array,guess,row5);
+        playAgain.setVisible(true);
+        quit.setVisible(true);
+        guessInput.clear();
+    }
+    public void colorBoxes (int[]anzeigeArray,String eingabe, Label[] row) {
+        //System.out.println(Arrays.toString(anzeigeArray));
+        Map<String, Button>buttonMap  = new HashMap<>() {{
+            put("A", A);
+            put("B", B);
+            put("C", C);
+            put("D", D);
+            put("E", E);
+            put("F", F);
+            put("G", G);
+            put("H", H);
+            put("I", I);
+            put("J", J);
+            put("K", K);
+            put("L", L);
+            put("M", M);
+            put("N", N);
+            put("O", O);
+            put("P", P);
+            put("Q", Q);
+            put("R", R);
+            put("S", S);
+            put("T", T);
+            put("U", U);
+            put("V", V);
+            put("W", W);
+            put("X", X);
+            put("Y", Y);
+            put("Z", Z);
+        }};
+        for (int i = 0; i < eingabe.length(); i++) {
+            row[i].setText(String.valueOf(eingabe.charAt(i)));
+
+
+            if (anzeigeArray[i] == 0) {
+
+                row[i].setStyle("-fx-background-color: #919191;");
+
+                if (buttonMap.get(row[i].getText()).getStyle().equals("-fx-background-color: #31a127;")){
+
+                } else if (buttonMap.get(row[i].getText()).getStyle().equals("-fx-background-color: #f0b63a;")){
+
+                } else {
+                    buttonMap.get(row[i].getText()).setStyle("-fx-background-color: #919191;");
+
+                }
+
+
+
+
+            } else {
+
+                if (anzeigeArray[i] == 1) {
+
+                    row[i].setStyle("-fx-background-color: #f0b63a;");
+
+                    if (buttonMap.get(row[i].getText()).getStyle().equals("-fx-background-color: #31a127;")) {
+
+                    } else {
+                        buttonMap.get(row[i].getText()).setStyle("-fx-background-color: #f0b63a;");
+
+                    }
+
+
+                } else {
+
+                    row[i].setStyle("-fx-background-color: #31a127;");
+
+                    buttonMap.get(row[i].getText()).setStyle("-fx-background-color: #31a127;");
+
+                }
+            }
+        }
+    }
+}
